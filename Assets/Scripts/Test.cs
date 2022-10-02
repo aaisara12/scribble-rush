84623bfcad74b92aadd88ae3f5677ba7c35cd4d6
aaisara12 <aisara121201@gmail.com>
2022-10-01 23:56:24 -0700

    Added new checkmark

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] UnityEngine.UI.Image image;
     void Start()
     {
         //GetComponent<StringChannelSender>().SendValue("Hello World!");
 
-        GetComponent<IntChannelSender>().SendValue(3);
+        GetComponent<IntChannelSender>()?.SendValue(3);
+    }
+
+    public void SetColor(Color color)
+    {
+        image.color = color;
     }
 
 }
