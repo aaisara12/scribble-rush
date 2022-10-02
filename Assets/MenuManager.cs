4bce7caecd7ce5e784d9ffb5659ad5ad9f93f92f
Aidan Strong <aidan.m.strong@gmail.com>
2022-10-02 13:55:16 -0700

    Changed Settings -> Gallery

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -11,16 +11,18 @@ public class MenuManager : MonoBehaviour
     public void quit(){
         Application.Quit();
     }
+    
     public void goToCarousel(){
         SceneManager.LoadScene(1);
     }
+
     public void goToCredits(){
         SceneManager.LoadScene(4);
     }
+
     public void goToGame(){
         SceneManager.LoadScene(0);
         SceneManager.LoadScene(3,LoadSceneMode.Additive);
-
     }
 
     public void openLink(string link){
